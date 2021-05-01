8d97f9567ad62d30dc7085609a0d68d11e1b67c7
Tree Davies <tdavies@darkphysics.net>
2021-05-01 14:30:42 -0700

    Update percentile Signed-off-by: Tree Davies <tdavies@darkphysics.net>

--- data-science/percentile.py
+++ data-science/percentile.py
@@ -1,16 +1,58 @@
 #!/usr/bin/python3
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2021 Tree Davies
 import numpy as np
 import sys
 
-dataset = []
-filename = "./" + str(sys.argv[1])
-with open(filename, "r") as fd:
-  lines = fd.readlines()
-  for ln in lines:
-    val = ln.lstrip().rstrip()
-    dataset.append(int(val))
 
-for p in range(1,101):
-  qth = str(np.percentile(dataset, p, interpolation='lower'))
-  print("P"+str(p)+": "+ qth )
+def make_dat(ds, name, stepping):
+	name = name + ".dat"
+	with open(name, "w") as ff:
+		for tup in ds:
+			p = tup[0]
+			metric = tup[1]
+			string = str(p) + " " + str(metric) + '\n'
+			ff.write(string)
+
+
+if __name__ == '__main__':
+	
+	dataset = []
+
+	if sys.argv[1] == '--fio' and sys.argv[2] != '':
+		filename = "./" + str(sys.argv[2])
+		with open(filename, "r") as fd:
+			lines = fd.readlines()
+			for ln in lines:
+				lst = ln.split(',')
+				metric = int(lst[1])
+				dataset.append(metric)
+	else:  
+		filename = "./" + str(sys.argv[1])
+		with open(filename, "r") as fd:
+			lines = fd.readlines()
+			for ln in lines:
+				val = ln.lstrip().rstrip()
+				dataset.append(int(val))
+
+
+	P = []
+
+	qth = str(np.percentile(dataset, 1, interpolation='lower'))
+	P.append((1,qth))
+	print("P"+str(1)+": "+ qth)
+
+	stepping = 5
+	for p in range(5, 96, stepping):
+		qth = str(np.percentile(dataset, p, interpolation='lower'))
+		P.append((p,qth))
+		print("P"+str(p)+": "+ qth)
+	qth = str(np.percentile(dataset, 99, interpolation='lower'))
+	P.append((99,qth))
+	print("P"+str(99)+": "+ qth)
+	make_dat(P, filename, stepping)
+
+	print("\nMin: " + str(min(dataset)))
+	print("Max: " + str(max(dataset)))
+
 
